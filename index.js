0f826d27cf6d1fd240cb3d9420c9b2fca96b7ad7
muhaiminultasin <muhaiminulislamtasin@gmail.com>
2024-05-30 22:09:43 +0600

    assignment done

---
+++ index.js
@@ -0,0 +1,95 @@
+// // Problem 1
+
+let carName = {
+    name : "Volvo",
+};
+
+
+// //Proble 2
+
+let firstName = "John", lastName = "Doe", age = 35;
+
+
+// // Problem 3
+
+ let x1 = 10;
+let y1 = 5;
+console.log(x1 *= y1);
+
+
+// problem 4 
+
+let length = 16;            // number
+const lastName2 = "Johnson"; //string
+const obj = {
+    firstName: "John",  
+    lastName: "Doe"
+  };                        //object
+
+
+console.log(typeof length);
+console.log(typeof lastName2);
+console.log(typeof obj);
+
+
+// Problem 5
+
+ 
+ function myFunction() {
+  alert("Hello World!");
+}
+myFunction()
+
+
+// Problem 6
+
+const person = {
+    name :"John",
+    age: 50,
+}
+
+alert(person.name + " is " + person.age);
+
+// Problem 7
+
+function btn(){
+    const button = document.getElementById("try");
+    button.style.backgroundColor="red";
+
+}
+
+
+// Problem 8.1
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
+
+
+// Problem 8.2
+const brand = ["Volvo", "Jeep", "Mercedes"];
+
+brand[0] = "ford";
+
+// console.log(brand);
+
+// Problem 9.1
+
+let random = Math.random();
+console.log(random)
+
+// Problem 9.2
+
+let max = Math.max(10, 20);
+
+console.log(max);
+
+
+// Problem 9.2
+let sqrt = Math.sqrt(9);
+console.log(sqrt);
+
+// Problem 10
+
+let x = 10;
+let y = 5;
+alert(x > y);
